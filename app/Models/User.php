875ee605d08ba65a1bba14da7019c725c72cb2b6
placidiasitorus <santasitorus105@gmail.com>
2025-12-08 00:48:54 +0700

    feat: customize users table and User model for roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Notifications\Notifiable; 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Authenticatable
